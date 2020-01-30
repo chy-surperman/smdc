66fd9dcbcc161f5fa5cdede07b8ce5489c352439
chy <chy>
2020-01-30 13:39:46 +0800

    chushihua

---
+++ src/test/java/com/chy/smdc/Logclass.java
@@ -0,0 +1,26 @@
+package com.chy.smdc;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class Logclass {
+
+    private final Logger logger = LoggerFactory.getLogger(Logclass.class);
+
+    @Test
+    public void test1() {
+        logger.debug("sssss");
+        logger.info("jkdhjsla");
+        logger.error("sdhsjkdksdsalkd");
+    }
+
+
+}
