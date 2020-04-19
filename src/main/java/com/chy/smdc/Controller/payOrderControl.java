e0908a63df07fcf2d0924a2681a8f779367c3781
chy <chy>
2020-04-19 11:45:50 +0800

    前端往后端传值修改

--- src/main/java/com/chy/smdc/Controller/payOrderControl.java
+++ src/main/java/com/chy/smdc/Controller/payOrderControl.java
@@ -14,14 +14,13 @@ import com.chy.smdc.util.messageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -58,26 +57,33 @@ public class payOrderControl {
     @Value("${application.sign_type}")
     public  String sign_type ;
 
+    @Value("${application.timeout_express}")
+    public  String timeout_express ;
 
+    @Value("${application.product_code}")
+    public  String product_code ;
 
     @Autowired
     payOrderServiceImpl payOrderService;
 
-
     @GetMapping("/pay")
-    public Result payOrderMeth(String price, String place, String name, String iphone, HttpServletResponse response) throws AlipayApiException, IOException {
+    public Result payOrderMeth(String price, String place, @RequestParam(value="selectFoods[]") String[] selectFoods, String name, String iphone, HttpServletResponse response) throws AlipayApiException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
-        System.out.println(place + "共计花费" + price + "-" + name + "-" + iphone);
+        for (String i: selectFoods ) {
+            System.out.println(i.toString());
+        }
         AlipayClient client = new DefaultAlipayClient(url,appid,privatekey,format,charset,publickey,sign_type);
         AlipayTradeWapPayRequest alipay_request=new AlipayTradeWapPayRequest();
         // 封装请求支付信息
         AlipayTradeWapPayModel model=new AlipayTradeWapPayModel();
-        model.setBody("我是"+name+"我的地址是"+place);
-        model.setSubject("我的电话"+iphone);
+        model.setBody("云店扫码自助点餐");
+        model.setSubject("云店扫码支付");
         model.setOutTradeNo(payOrderService.OrderNum());
-        model.setTimeoutExpress("2m");
+        System.out.println(payOrderService.OrderNum());
+        System.out.println(payOrderService.OrderNum());
+        model.setTimeoutExpress(timeout_express);
         model.setTotalAmount(price);
-        model.setProductCode("QUICK_WAP_WAY");
+        model.setProductCode(product_code);
         alipay_request.setBizModel(model);
         // form表单生产
         String form = "";
